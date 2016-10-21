199e7ebebf72bb133a3f5b7be6f14c9a328c150c
yash248 <dabhi.yash.r@gmail.com>
2016-10-21 15:24:24 -0700

    Add files via upload

--- form-vlidation.js
+++ form-vlidation.js
@@ -4,15 +4,10 @@ $(function() {
   $("form[name='counter']").validate({
     // Specify validation rules
     rules: {
-      // The key name on the left side is the name attribute
-      // of an input field. Validation rules are defined
-      // on the right side
       num: "required",
-     
     },
     ,
-    // Make sure the form is submitted to the destination defined
-    // in the "action" attribute of the form when valid
+    
     submitHandler: function(form) {
       form.start();
     }
